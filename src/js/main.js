2f6f3bfd15a69c8da8def776e4690b76d2c7d593
elisavonderforst <Elisa.vonderforst@studentfr.ch>
2024-04-22 16:56:13 +0200

    plein d'autre truc

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-var exercerButton = document.querySelector(".button-exercer");
-var exercerPopUp = document.querySelector(".exercer-popup");
-var exercerCroix = document.querySelector(".exercer-img-croix");
+var exercerButton = document.querySelector(".button-train");
+var exercerPopUp = document.querySelector(".train-popup");
+var exercerCroix = document.querySelector(".train-img-croix");
 
 var changeButton = function () {
   exercerPopUp.classList.toggle("is-visible");
@@ -20,3 +20,25 @@ var changeBackground = function () {
 };
 
 exercerButton.addEventListener("click", changeBackground);
+
+// test
+
+var checkAllTrigger = document.querySelector(".check-all");
+var checkboxes = document.querySelectorAll(".button-coche");
+var uncheckAllTrigger = document.querySelector(".uncheck-all");
+
+var checkAll = function () {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = true;
+  });
+};
+
+checkAllTrigger.addEventListener("click", checkAll);
+
+var uncheckAll = function () {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
+};
+
+uncheckAllTrigger.addEventListener("click", uncheckAll);
